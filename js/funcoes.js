ba80f4a72291d17db1ac4814905634129922961e
Felipe Prodossimo <felipe.profissional4@gmail.com>
2020-12-07 22:23:43 -0300

    Mais 1 html e js acrescentados

---
+++ js/funcoes.js
@@ -0,0 +1,19 @@
+console.log("Hello everybody!!");
+
+let name = prompt("What's your name?");
+alert("Hello everybody!! " + name + " seja bem vindo!");
+
+let numero1 = prompt("Digite uma idade");
+let numero2 = prompt("Digite outra idade");
+
+alert(numero1 = numero2);
+
+if (numero1 > 20){
+    alert("Número é maior que 20");
+}
+
+else{
+    alert("Número é menor que 20");
+}
+
+alert(numero1 * numero2);
